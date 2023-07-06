3205a5668464f99aaf931db4ff2295e2a8fac975
aloric97 <leandro.tombetta87@gmail.com>
2023-07-06 00:55:28 -0300

    nueva version

---
+++ src/main/java/com/jwt/implementation/Dto/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.jwt.implementation.Dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/jwt/implementation/Service/JwtService.java
@@ -0,0 +1,27 @@
+package com.jwt.implementation.Service;
+
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
+/*
+@Component
+public class JwtService {
+
+
+    public String generateToken(String userName){
+        Map<String, Object> claims= new HashMap<>();
+
+        return createToken(claims,userName);
+    }
+
+    private String createToken(Map<String,Object> claims,String userName){
+
+    }
+}
+
+ */
